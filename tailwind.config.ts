92d32712e164850f02a5391d98e76889119e4d8b
Ivan Buljovcic <ivan.buljovcic@gmail.com>
2025-03-05 23:22:34 +0100

    css config; cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from "tailwindcss";
-import tailwindAnimate from "tailwindcss-animate"
+// import tailwindAnimate from "tailwindcss-animate"
 
 export default {
-  darkMode: 'class',
+  // darkMode: 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [tailwindAnimate],
+  // plugins: [tailwindAnimate],
 } satisfies Config;
