a670bd92aeaa4e2ebf39edcf8fbff7d8a629c32b
Ivan Buljovčić <ivan.buljovcic@gmail.com>
2025-03-06 15:33:16 +0100

    chore: update app title and add npm to Volta configuration; adjust search component props handling

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,17 @@
 import SearchProducts from "@/components/Search/search-products";
 
-const Search = async ({ params }: { params: Promise<{ query: string }> }) => {
-  const { query } = await params;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const Search = async (props: any) => {
+  // Props set as any
+  // 
+  // previous definition of props as { searchParams: { query: string }}
+  // was causing the build to fail due to missmatch of generated types
+  // 
+  // after investigating for more then an hour,
+  // this solution with `any` is a bandage until a proper solution
+  // of the issue can be understood and found
+
+  const { query } = await props.searchParams!
 
   return (
     <div className="flex flex-col items-center bg-gray-100 p-4 min-h-screen align-top">
