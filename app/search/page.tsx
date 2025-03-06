c319f6aff8efef6019025fcc25f43f6f349e2cd5
Ivan Buljovčić <ivan.buljovcic@gmail.com>
2025-03-06 16:32:13 +0100

    fix: update Search component props type to PageProps for better type safety

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,17 +1,8 @@
+import { PageProps } from "@/.next/types/app/search/page";
 import SearchProducts from "@/components/Search/search-products";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const Search = async (props: any) => {
-  // Props set as any
-  // 
-  // previous definition of props as { searchParams: { query: string }}
-  // was causing the build to fail due to missmatch of generated types
-  // 
-  // after investigating for more then an hour,
-  // this solution with `any` is a bandage until a proper solution
-  // of the issue can be understood and found
-
-  const { query } = await props.searchParams!
+const Search = async (props: PageProps) => {
+  const { query } = await props.searchParams;
 
   return (
     <div className="flex flex-col items-center bg-gray-100 p-4 min-h-screen align-top">
