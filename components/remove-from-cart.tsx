c0ce77871a552d88eda7acc717d153206f28ae7c
Ivan Buljovčić <ivan.buljovcic@gmail.com>
2025-03-06 12:54:33 +0100

    fix: correct button class formatting in remove from cart component

--- components/remove-from-cart.tsx
+++ components/remove-from-cart.tsx
@@ -14,7 +14,7 @@ const RemoveFromCart = ({ product }: RemoveFromCartProps) => {
   return (
     <button
       onClick={() => dispatch(removeItem(product))}
-      className="space-x-2 bg-red-700 disabled:bg-gray-500 hover:opacity-50 ml-auto px-4 py-2 rounded font-bold text-button-foreground transition-opacity cursor-pointer cursor-pointer disabled:cursor-not-allowed shrink-0"
+      className="space-x-2 bg-red-700 disabled:bg-gray-500 hover:opacity-50 ml-auto px-4 py-2 rounded font-bold text-button-foreground transition-opacity cursor-pointer disabled:cursor-not-allowed shrink-0"
     >
       Remove item from cart
     </button>
